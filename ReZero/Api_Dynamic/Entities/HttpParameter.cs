2a0863f33a504f254c4694c4bdc0343c87db1fba
sunkaixuan <610262374@qq.com>
2023-10-29 03:28:44 +0800

    -

---
+++ ReZero/Api_Dynamic/Entities/HttpParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReZero 
+{
+    public class HttpParameter
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
+}
