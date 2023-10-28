cc00a1e8740004f18021a0d47c39642b4eb51c23
sunkaixuan <610262374@qq.com>
2023-10-29 04:23:34 +0800

    添加注释和优化类名

--- ReZero/Api_Dynamic/ApiHelper.cs
+++ ReZero/Api_Dynamic/ApiHelper.cs
@@ -1,19 +1,35 @@
 ﻿using Microsoft.AspNetCore.Http;
 using ReZero.API.RequestHandler;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace ReZero 
+namespace ReZero
 {
+    /// <summary>
+    /// Helper class for handling API requests and determining appropriate request method handlers.
+    /// </summary>
     internal class ApiHelper
     {
-        public  bool IsHttpMethod(string requestMethodString, out HttpRequestMethod requestMethod)
+        /// <summary>
+        /// Determines if the provided string represents a valid HTTP request method.
+        /// </summary>
+        /// <param name="requestMethodString">The string representing the HTTP request method.</param>
+        /// <param name="requestMethod">The parsed HttpRequestMethod enum value.</param>
+        /// <returns>True if the string represents a valid HTTP request method; otherwise, false.</returns>
+        public bool IsHttpMethod(string requestMethodString, out HttpRequestMethod requestMethod)
         {
+            // Try to parse the request method string into HttpRequestMethod enum.
             return Enum.TryParse<HttpRequestMethod>(requestMethodString, ignoreCase: true, out requestMethod);
         }
+
+        /// <summary>
+        /// Gets the appropriate request method handler based on the provided HTTP request method.
+        /// </summary>
+        /// <param name="method">The parsed HttpRequestMethod enum representing the HTTP request method.</param>
+        /// <param name="context">The HttpContext associated with the request.</param>
+        /// <returns>An instance of the appropriate request method handler.</returns>
         public IRequestMethodHandler GetHandler(HttpRequestMethod method, HttpContext context)
         {
+            // Determine the request method and return the corresponding handler.
             switch (method)
             {
                 case HttpRequestMethod.GET:
@@ -27,8 +43,10 @@ namespace ReZero
                 case HttpRequestMethod.PATCH:
                     return new PatchRequestHandler(context);
                 default:
+                    // Throw an exception if the request method is not supported.
                     throw new NotSupportedException("Unsupported HTTP request method");
             }
         }
     }
 }
+ 
