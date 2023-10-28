eb1f5f02b9a3523de84cab81e816dd0600050015
sunkaixuan <610262374@qq.com>
2023-10-29 04:20:20 +0800

    添加注释

--- ReZero/Initialization/MyServiceCollectionExtensions.cs
+++ ReZero/Initialization/MyServiceCollectionExtensions.cs
@@ -2,23 +2,34 @@
 using Microsoft.Extensions.DependencyInjection;
 using ReZero.Api_ReZero.Interface;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ReZero
 {
+    /// <summary>
+    /// Extension methods for configuring ReZero services in IServiceCollection.
+    /// </summary>
     public static class MyServiceCollectionExtensions
     {
-        public static IServiceCollection ReZero(this IServiceCollection services, ReZeroOptions? options=null)
+        /// <summary>
+        /// Adds ReZero services to the specified IServiceCollection.
+        /// </summary>
+        /// <param name="services">The IServiceCollection to which ReZero services are added.</param>
+        /// <param name="options">Optional ReZero options.</param>
+        /// <returns>The updated IServiceCollection.</returns>
+        public static IServiceCollection ReZero(this IServiceCollection services, ReZeroOptions? options = null)
         {
+            // If options are not provided, create a new instance of ReZeroOptions.
             options = options ?? new ReZeroOptions();
 
-
+            // Add services to the IServiceCollection.
             services.AddTransient<IDynamicApi, DynamicApi>();
             services.AddTransient<IReZeroApi, ReZeroApi>();
-            services.AddTransient<IStartupFilter,RequestSetOptionsStartupFilter>();
+            services.AddTransient<IStartupFilter, RequestSetOptionsStartupFilter>();
+
+            // Create an instance of ORM with the specified connection configuration and add it as a transient service.
             services.AddTransient<ORM>(it => new ORM(options.ConnectionConfig));
 
+            // Return the updated IServiceCollection.
             return services;
         }
     }
