8019260b4f4356fb9907634aaf56ae7100097efe
sunkaixuan <610262374@qq.com>
2023-10-30 16:10:23 +0800

    -

--- ReZero/Database/DbModels/DiffLog.cs
+++ ReZero/Database/DbModels/DiffLog.cs
@@ -4,14 +4,14 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ReZero 
+namespace ReZero.Database.DbModels
 {
-    public class DiffLog:DbReZeroBase
+    public class DiffLog : DbReZeroBase
     {
         public string TableName { get; set; }
-        [SugarColumn(IsJson =true)]
+        [SugarColumn(IsJson = true)]
         public JArray BeforeData { get; set; }
         [SugarColumn(IsJson = true)]
-        public JArray AfterData { get; set; } 
+        public JArray AfterData { get; set; }
     }
 }
--- ReZero/Database/DbModels/InterfaceList.cs
+++ ReZero/Database/DbModels/InterfaceList.cs
@@ -3,11 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ReZero
+namespace ReZero.Database.DbModels
 {
-    public class InterfaceList: DbReZeroBase
+    public class InterfaceList : DbReZeroBase
     {
-        public string Name { get; set; }  
+        public string Name { get; set; }
         public string Description { get; set; }
         public HttpRequestMethod HttpMethod { get; set; }
         public List<InterfaceParameter> Parameters { get; set; }
