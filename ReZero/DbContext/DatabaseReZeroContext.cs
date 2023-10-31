d2956e86ee5b711ce269876f1237e77313fedf07
sunkaixuan <610262374@qq.com>
2023-11-01 06:55:24 +0800

    系统表添加下划线

--- ReZero/DbContext/DatabaseReZeroContext.cs
+++ ReZero/DbContext/DatabaseReZeroContext.cs
@@ -18,9 +18,30 @@ namespace ReZero
         /// </summary>
         /// <param name="connectionConfig">Database connection configuration.</param>
         public DatabaseReZeroContext(ConnectionConfig connectionConfig)
-        { 
+        {
+            SetConfigureExternalServices(connectionConfig);
+
+            SetAop(connectionConfig);
+
             // Create a new SqlSugar client instance using the provided connection configuration.
             SugarClient = new SqlSugarClient(connectionConfig);
+        }
+
+        private static void SetAop(ConnectionConfig connectionConfig)
+        {
+            connectionConfig.ConfigureExternalServices.EntityService = (x, p) =>
+            {
+                p.DbColumnName = UtilMethods.ToUnderLine(p.DbColumnName, true);
+            };
+            connectionConfig.ConfigureExternalServices.EntityNameService = (x, p) =>
+            {
+                p.DbTableName = UtilMethods.ToUnderLine(p.DbTableName, true);
+            };
+        }
+
+        private static void SetConfigureExternalServices(ConnectionConfig connectionConfig)
+        {
+            connectionConfig.ConfigureExternalServices = connectionConfig.ConfigureExternalServices ?? new ConfigureExternalServices();
         }
     }
 }
