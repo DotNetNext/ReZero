5f71cedf040b2b836ef599271c3f703b28fd3d90
sunkaixuan <610262374@qq.com>
2023-11-05 16:18:17 +0800

    -

--- ReZero/Database/Databases/UserInfo.cs
+++ ReZero/Database/Databases/UserInfo.cs
@@ -7,7 +7,7 @@ namespace ReZero
     public class ZeroUserInfo : DbBase
     {
         public string? UserId { get; set; }
-        public string? Username { get; set; }
+        public string? UserName { get; set; }
         public string? Password { get; set; }
         public bool IsMasterAdmin { get; set; }
     }
