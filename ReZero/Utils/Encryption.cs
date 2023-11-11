0c5debf3b0c79b01d82200e295c4292530ae51ad
sunkaixuan <610262374@qq.com>
2023-11-12 06:51:38 +0800

    添加注释

--- ReZero/Utils/Encryption.cs
+++ ReZero/Utils/Encryption.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
-
 namespace ReZero
 {
     public class Encryption
     {
+        /// <summary>
+        /// Encrypt the input string by shifting each character by 3 positions in the Unicode table.
+        /// </summary>
+        /// <param name="input">The string to encrypt</param>
+        /// <returns>The encrypted string</returns>
         public static string Encrypt(string input)
         {
             char[] inputArray = input.ToCharArray();
@@ -16,7 +21,11 @@ namespace ReZero
             }
             return new string(inputArray);
         }
-
+        /// <summary>
+        /// Decrypt the input string by shifting each character back by 3 positions in the Unicode table.
+        /// </summary>
+        /// <param name="input">The string to decrypt</param>
+        /// <returns>The decrypted string</returns>
         public static string Decrypt(string input)
         {
             char[] inputArray = input.ToCharArray();
@@ -29,3 +38,4 @@ namespace ReZero
         }
     }
 }
+ 
