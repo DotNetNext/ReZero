9786ad70ec6cfa853e16940f3bb0d7ef39bc974c
sunkaixuan <610262374@qq.com>
2023-11-13 16:11:24 +0800

    -

--- ReZero/Database/API/InterfaceCategory.cs
+++ ReZero/Database/API/InterfaceCategory.cs
@@ -12,8 +12,8 @@ namespace ReZero
         public long? ParentId{get;set;}
         [SugarColumn(IsNullable =true)]
         public string? Description { get; set; }
-        [SugarColumn(IsIgnore =true)]
-        public List<ZeroInterfaceCategory>? SubInterfaceCategories { get; set; }
-        public string? Url { get;  set; }
+        [SugarColumn(IsIgnore = true)]
+        public List<ZeroInterfaceCategory>? SubInterfaceCategories { get; set; };
+        public string? Url { get; set; } = "#";
     }
 }
