12711d32fb107f5faa16a7e115f248923705c183
sunkaixuan <610262374@qq.com>
2023-11-14 13:25:30 +0800

    修改方法名

--- ReZero/DbContext/DatabaseContext.cs
+++ ReZero/DbContext/DatabaseContext.cs
@@ -19,34 +19,46 @@ namespace ReZero
         /// <param name="connectionConfig">Database connection configuration.</param>
         public DatabaseContext(ConnectionConfig connectionConfig)
         {
-            SetConfigureExternalServices(connectionConfig);
+            InitializeExternalServices(connectionConfig);
 
             ConfigureExternalServices(connectionConfig);
 
             // Create a new SqlSugar client instance using the provided connection configuration.
-            SugarClient = new SqlSugarClient(connectionConfig, db => 
+            SugarClient = new SqlSugarClient(connectionConfig, db =>
             {
-                db.QueryFilter.AddTableFilter<IDeleted>(it=>it.IsDeleted==false);
+                db.QueryFilter.AddTableFilter<IDeleted>(it => it.IsDeleted == false);
                 db.Aop.OnLogExecuting = (s, p) => UtilMethods.GetNativeSql(s, p);
-            }); 
+            });
 
-            
+
         }
 
+        /// <summary>
+        /// Configures external services for the provided database connection configuration.
+        /// </summary>
+        /// <param name="connectionConfig">Database connection configuration.</param>
         private static void ConfigureExternalServices(ConnectionConfig connectionConfig)
         {
             connectionConfig.ConfigureExternalServices.EntityService = (x, p) =>
             {
+                // Convert the database column name to snake case.
                 p.DbColumnName = UtilMethods.ToUnderLine(p.DbColumnName);
             };
             connectionConfig.ConfigureExternalServices.EntityNameService = (x, p) =>
             {
+                // Convert the database table name to snake case.
                 p.DbTableName = UtilMethods.ToUnderLine(p.DbTableName);
             };
         }
 
-        private static void SetConfigureExternalServices(ConnectionConfig connectionConfig)
+        /// <summary>
+        /// Initializes external services for the provided database connection configuration.
+        /// </summary>
+        /// <param name="connectionConfig">Database connection configuration.</param>
+        private static void InitializeExternalServices(ConnectionConfig connectionConfig)
         {
+            // Adds comments to explain the purpose of the method.
+            // Sets the ConfigureExternalServices property of the provided connection configuration to a new instance of ConfigureExternalServices if it is null.
             connectionConfig.ConfigureExternalServices = connectionConfig.ConfigureExternalServices ?? new ConfigureExternalServices();
         }
     }
