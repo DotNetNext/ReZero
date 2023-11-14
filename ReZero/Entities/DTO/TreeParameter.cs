59155c95ee1025a061c178f22297f90cbdd23b32
sunkaixuan <610262374@qq.com>
2023-11-14 13:08:26 +0800

    添加注释

--- ReZero/Entities/DTO/TreeParameter.cs
+++ ReZero/Entities/DTO/TreeParameter.cs
@@ -4,11 +4,29 @@ using System.Text;
 
 namespace ReZero 
 {
+    /// <summary>
+    /// Represents the parameters for a tree structure.
+    /// </summary>
     public class TreeParameter
     {
-        public string?  CodePropertyName { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the property that contains the code.
+        /// </summary>
+        public string? CodePropertyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the root value of the tree.
+        /// </summary>
         public object? RootValue { get; set; }
-        public string?  ParentCodePropertyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the property that contains the parent code.
+        /// </summary>
+        public string? ParentCodePropertyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the property that contains the child nodes.
+        /// </summary>
         public string? ChildPropertyName { get; set; }
     }
 }
