0e73b565c5f1802fbadc692152d5297f7413dfc5
sunkaixuan <610262374@qq.com>
2023-11-15 01:05:17 +0800

    添加注释

--- ReZero/Ui/DefaultUi/DefaultUiManager.cs
+++ ReZero/Ui/DefaultUi/DefaultUiManager.cs
@@ -10,7 +10,7 @@ namespace ReZero
     /// 默认UI使用，如果是Vue前后分离不会使用该类
     /// </summary>
     public class DefaultUiManager : IUiManager
-    { 
+    {
         private readonly string masterPagePlaceholder = "@@master_page.html";
         private readonly string masterPageFolder = "template";
         private readonly string masterPageFileName = "master_page.html";
@@ -18,27 +18,43 @@ namespace ReZero
         private readonly string masterMenuPlaceholder = "@@left-menu";
 
         public DefaultUiManager()
-        { 
+        {
         }
 
+        /// <summary>
+        /// Replaces the master page placeholder with the actual master page HTML content and replaces the layout content placeholder with the modified file content.
+        /// </summary>
+        /// <param name="fileContent">The content of the file to modify.</param>
+        /// <param name="filePath">The path of the file to modify.</param>
+        /// <returns>The modified file content.</returns>
         public async Task<string> GetHtmlAsync(string fileContent, string filePath)
-        {  
+        {
             var modifiedContent = fileContent.Replace(masterPagePlaceholder, "");
             var masterPagePath = Path.Combine(Path.GetDirectoryName(filePath), masterPageFolder, masterPageFileName);
             var masterPageHtml = await File.ReadAllTextAsync(masterPagePath);
-            var menuList =await App.Db.Queryable<ZeroInterfaceCategory>().ToTreeAsync(it => it.SubInterfaceCategories, it => it.ParentId, 0, it => it.Id);
-            
+            var menuList = await App.Db.Queryable<ZeroInterfaceCategory>().ToTreeAsync(it => it.SubInterfaceCategories, it => it.ParentId, 0, it => it.Id);
+
             var menuHtml = await GetMenuHtml(menuList);
-            masterPageHtml=masterPageHtml.Replace(masterMenuPlaceholder, menuHtml);
+            masterPageHtml = masterPageHtml.Replace(masterMenuPlaceholder, menuHtml);
             modifiedContent = masterPageHtml.Replace(layoutContentPlaceholder, modifiedContent);
             return modifiedContent;
         }
 
-        public async Task<string> GetMenuHtml(List<ZeroInterfaceCategory> categories) 
+        /// <summary>
+        /// Generates the HTML code for the menu based on the given list of interface categories.
+        /// </summary>
+        /// <param name="categories">The list of interface categories.</param>
+        /// <returns>The HTML code for the menu.</returns>
+        public async Task<string> GetMenuHtml(List<ZeroInterfaceCategory> categories)
         {
             return await Task.FromResult(MenuBuilder.GenerateMenu(categories));
         }
 
+        /// <summary>
+        /// Determines whether the given file content contains the master page placeholder.
+        /// </summary>
+        /// <param name="fileContent">The content of the file to check.</param>
+        /// <returns>True if the file content contains the master page placeholder, otherwise false.</returns>
         public bool IsMasterPage(string fileContent)
         {
             return fileContent.Contains(masterPagePlaceholder);
