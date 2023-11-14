5bfc4d317a2ede86cbed7ff84d9127fd2640f44d
sunkaixuan <610262374@qq.com>
2023-11-14 13:09:18 +0800

    添加注释

--- ReZero/Entities/DTO/CommonPage.cs
+++ ReZero/Entities/DTO/CommonPage.cs
@@ -4,10 +4,24 @@ using System.Text;
 
 namespace ReZero 
 {
+    /// <summary>
+    /// Represents a common page with page number, page size, and total count.
+    /// </summary>
     public class CommonPage
     {
+        /// <summary>
+        /// Gets or sets the page number.
+        /// </summary>
         public int PageNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page size.
+        /// </summary>
         public int PageSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total count.
+        /// </summary>
         public int Total { get; set; }
     }
 }
