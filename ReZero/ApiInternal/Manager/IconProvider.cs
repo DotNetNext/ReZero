6a89c2c801fe1baf01a90e01ba202b23663e830a
sunkaixuan <610262374@qq.com>
2023-11-18 01:10:33 +0800

    种子优化

--- ReZero/ApiInternal/Manager/IconProvider.cs
+++ ReZero/ApiInternal/Manager/IconProvider.cs
@@ -4,18 +4,8 @@ using System.Text;
 
 namespace ReZero 
 {
-    internal class IconProvider
-    {
-        internal const int Id1 = 1;
-        internal const int Id2 = 2;
-        internal const int Id3 = 3;
-        internal const int Id4 = 4;
-
-        private const string IconName1 = "mdi mdi-home";
-        private const string IconName2 = "mdi polymer";
-        private const string IconName3 = "mdi mdi-book-open";
-        private const string IconName4 = "mdi-arrange-send-backward";
-
+    public partial class IconProvider
+    {  
         internal List<ZeroIconData> GetDatas()
         {
             List<ZeroIconData> datas = new List<ZeroIconData>();
---
+++ ReZero/DatabaseSeeder/IconProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReZero 
+{
+    public partial class IconProvider
+    {
+        internal const int Id1 = 1;
+        internal const int Id2 = 2;
+        internal const int Id3 = 3;
+        internal const int Id4 = 4;
+
+        private const string IconName1 = "mdi mdi-home";
+        private const string IconName2 = "mdi polymer";
+        private const string IconName3 = "mdi mdi-book-open";
+        private const string IconName4 = "mdi-arrange-send-backward";
+
+    }
+}
