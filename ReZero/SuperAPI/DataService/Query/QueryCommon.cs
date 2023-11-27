b88bbf8efae8892dc4d03bba43c3fa242e704647
sunkaixuan <610262374@qq.com>
2023-11-27 23:32:56 +0800

    修改公用参数报错

--- ReZero/SuperAPI/DataService/Query/QueryCommon.cs
+++ ReZero/SuperAPI/DataService/Query/QueryCommon.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq; 
+using System.Linq;
+using System.Data;
 namespace ReZero.SuperAPI
 {
     internal class QueryCommon : IDataService
@@ -44,6 +45,7 @@ namespace ReZero.SuperAPI
             {
                 foreach (var item in dataModel.WhereParameters.Where(it=>(it.Value+"")!=""))
                 {
+                    item.Name = App.Db.EntityMaintenance.GetDbColumnName(item.Name,queryObject.EntityType);
                     switch (item.FieldOperator) 
                     {
                         case FieldOperatorType.Equal:
