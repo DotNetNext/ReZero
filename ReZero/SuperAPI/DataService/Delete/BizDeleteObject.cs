9441e024964da886f485c69e29e866f7e1cb6fb6
sunkaixuan <610262374@qq.com>
2023-12-05 21:22:14 +0800

    添加逻辑删除动态配置

---
+++ ReZero/SuperAPI/DataService/Delete/BizDeleteObject.cs
@@ -0,0 +1,27 @@
+﻿using Kdbndp.TypeHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReZero.SuperAPI 
+{
+    internal class BizDeleteObject
+    {
+        public async Task<object> ExecuteAction(DataModel dataModel)
+        {
+            var db = App.Db;
+            var type = await EntityGeneratorManager.GetTypeAsync(dataModel.TableId);
+            var entity = db.EntityMaintenance.GetEntityInfo(type);
+            if (!entity.Columns.Any(it => it.PropertyName?.ToLower() == "isdeleted")) 
+            {
+                throw new Exception(TextHandler.GetCommonTexst(type .Name+ "没有IsDeleted属性不能逻辑删除", type.Name + "Cannot be logically deleted without IsDeleted attribute"));
+            }
+            await db.UpdateableByObject(dataModel.Data)
+                    .UpdateColumns("isdeleted")
+                    .ExecuteCommandAsync();
+            return true;
+        }
+    }
+}
