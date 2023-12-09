2300eeab1418119108392596f1b0671bfae65874
sunkaixuan <610262374@qq.com>
2023-12-09 21:08:35 +0800

    添加插入和更新

---
+++ SuperAPI/wwwroot/ReZero/default_ui/js/tools.js
@@ -0,0 +1,14 @@
+﻿var tools = { 
+    formToJson: function (id) {
+        var formData = {};
+        var formElements = document.getElementById(id).elements;
+
+        for (var i = 0; i < formElements.length; i++) {
+            var element = formElements[i];
+            if (element.name && element.value) {
+                formData[element.name] = element.value;
+            }
+        }
+        return formData;
+    }
+}
