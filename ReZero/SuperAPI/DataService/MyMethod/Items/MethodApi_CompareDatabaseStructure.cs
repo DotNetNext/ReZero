e31451624c7192f7ea06439893963b6844a4062a
sunkaixuan <610262374@qq.com>
2023-12-29 02:18:51 +0800

    更新表添加结构对比

--- ReZero/SuperAPI/DataService/MyMethod/Items/MethodApi_CompareDatabaseStructure.cs
+++ ReZero/SuperAPI/DataService/MyMethod/Items/MethodApi_CompareDatabaseStructure.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SqlSugar;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +7,41 @@ namespace ReZero.SuperAPI
 {
     public partial class MethodApi
     { 
-        public object CompareDatabaseStructure(string columns)
+        public object CompareDatabaseStructure(string [] ids)
         {
-            return "";
+            List<string> tableDifferences = new List<string>();
+            var result = string.Empty; 
+            var dbRoot = App.Db;
+            var entities=dbRoot.Queryable<ZeroEntityInfo>().In(ids).ToList();
+            var types = new Type[] { };
+            foreach (var entity in entities)
+            {
+                var codeFirstDb =App.GetDbById(entity.DataBaseId) ?? App.Db;
+                var type = EntityGeneratorManager.GetTypeAsync(entity.Id).GetAwaiter().GetResult();
+                if (codeFirstDb.DbMaintenance.IsAnyTable(codeFirstDb.EntityMaintenance.GetTableName(type),false))
+                {
+                    var diff = codeFirstDb.CodeFirst.GetDifferenceTables(type).ToDiffString();
+                    if (diff != null && !diff.Contains("No change"))
+                    {
+                        tableDifferences.Add(diff);
+                    }
+                }
+            }
+            if (tableDifferences.Count == 0)
+            {
+                result = $"<span class='diff_bule diff_success'>{TextHandler.GetCommonTexst("此操作没有风险，可以继续！！", "This operation is not risky and can continue!!")}</span>";
+            }
+            else
+            {
+                result = string.Join("", tableDifferences).Replace("\n", "<br>");
+            }
+            result = result.Replace("<br>----", "<h3 class='diff_h3'>");
+            result = result.Replace("----", "</h3>");
+            result = result.Replace("Table:", $"{TextHandler.GetCommonTexst("表名","Table")}:");
+            result = result.Replace("Add column", $"<span class='diff_bule'>{TextHandler.GetCommonTexst("添加列", "Add column")}</span>");
+            result = result.Replace("Update column", $"<span class='diff_yellow'>{TextHandler.GetCommonTexst("更新列", "Update column")}</span>");
+            result = result.Replace("Delete column", $"<span class='diff_red'>{TextHandler.GetCommonTexst("删除列", "Delete column")}</span>");
+            return result;
         }
     }
 }
