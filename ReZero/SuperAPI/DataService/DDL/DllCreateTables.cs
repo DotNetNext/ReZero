3134b2ac630d694df2bf2a8f26934cc481a5dcdb
sunkaixuan <610262374@qq.com>
2023-12-30 04:18:00 +0800

    完成创建表操作

--- ReZero/SuperAPI/DataService/DDL/DllCreateTables.cs
+++ ReZero/SuperAPI/DataService/DDL/DllCreateTables.cs
@@ -11,7 +11,18 @@ namespace ReZero.SuperAPI
         public async Task<object> ExecuteAction(DataModel dataModel)
         {
             await Task.Delay(0);
-            var connection = dataModel.DefaultParameters.First().Value;
+            var value = dataModel.DefaultParameters.First().Value; 
+            var dbRoot = App.Db;
+            var ids = dbRoot.Utilities.DeserializeObject<List<long>>(value+"");
+            List<string> tableDifferences = new List<string>();
+            var result = string.Empty;
+            var entities = dbRoot.Queryable<ZeroEntityInfo>().In(ids).ToList();
+            foreach (var entity in entities)
+            {
+                var codeFirstDb = App.GetDbById(entity.DataBaseId) ?? App.Db;
+                var type = EntityGeneratorManager.GetTypeAsync(entity.Id).GetAwaiter().GetResult();
+                 codeFirstDb.CodeFirst.InitTables(type);
+            }
             return true;
         }
     }
