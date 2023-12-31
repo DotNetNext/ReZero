a131d239c4d129b48f337d3c759a5d3aa9d200bb
sunkaixuan <610262374@qq.com>
2024-01-01 03:33:04 +0800

    导入实体逻辑

--- ReZero/SuperAPI/DataService/MyMethod/Items/MethodApi_ImportEntities.cs
+++ ReZero/SuperAPI/DataService/MyMethod/Items/MethodApi_ImportEntities.cs
@@ -11,16 +11,53 @@ namespace ReZero.SuperAPI
     {
         public object ImportEntities(long databasdeId, List<string> tableNames)
         {
-            var db=App.GetDbById(databasdeId)!;
+            var db = App.GetDbById(databasdeId)!;
+            List<EntityInfo> entityInfos = new List<EntityInfo>();
             foreach (var tableName in tableNames)
             {
-               var columns= db.DbMaintenance.GetColumnInfosByTableName(tableName);
-               var dataTable=db.Queryable<object>().AS(tableName).Where("0="+ PubConst.Random.Next(1,9999999)).ToDataTable();
-               var dbColumns = dataTable.Columns.Cast<DataColumn>().ToList();
-                
-               
+                EntityInfo entityInfo = CreateEntityInfo(db, tableName); 
+                entityInfos.Add(entityInfo);
             }
             return true;
         }
+
+        private static EntityInfo CreateEntityInfo(SqlSugarClient db, string tableName)
+        {
+            EntityInfo entityInfo = new EntityInfo();
+            var columns = db.DbMaintenance.GetColumnInfosByTableName(tableName, false);
+            var dataTable = db.Queryable<object>().AS(tableName).Where(GetWhereFalse()).ToDataTable();
+            var dtColumns = dataTable.Columns.Cast<DataColumn>().ToList();
+            var joinedColumns = columns.
+                Join(dtColumns, c =>
+                c.TableName,
+                dtc => dtc.ColumnName, (c, dtc) =>
+                new
+                {
+                    ColumnName = c.TableName,
+                    DataType = c.DataType,
+                    PropetyInfo = dtc.DataType,
+                    IsNullable = c.IsNullable
+                });
+            List<ZeroEntityColumnInfo> zeroEntityColumns = new List<ZeroEntityColumnInfo>();
+            foreach (var item in joinedColumns)
+            {
+                ZeroEntityColumnInfo zeroEntityColumnInfo = CreateEntityColumn(item);
+                zeroEntityColumns.Add(zeroEntityColumnInfo);
+            }
+            return entityInfo;
+        }
+        private static ZeroEntityColumnInfo CreateEntityColumn(object item)
+        {
+            ZeroEntityColumnInfo zeroEntityColumnInfo = new ZeroEntityColumnInfo();
+            return zeroEntityColumnInfo;
+        }
+
+        #region Helper
+        private static string GetWhereFalse()
+        {
+            return "0=" + PubConst.Random.Next(1, 9999999);
+        }
+
+        #endregion
     }
 }
