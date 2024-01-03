7ab6ad800066b23627c8b8fb5f1eb0b2788b9f7b
sunkaixuan <610262374@qq.com>
2024-01-03 14:27:25 +0800

    优化代码

--- ReZero/SuperAPI/Utils/JsonHelper.cs
+++ ReZero/SuperAPI/Utils/JsonHelper.cs
@@ -2,42 +2,72 @@
 using System.Collections.Generic;
 using System.Text;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters; 
+using Newtonsoft.Json.Converters;
 
 
-namespace ReZero.SuperAPI 
-{ 
+namespace ReZero.SuperAPI
+{
+    /// <summary>
+    /// Provides helper methods for JSON serialization and deserialization.
+    /// </summary>
     public static class JsonHelper
     {
         private static readonly JsonSerializerSettings DefaultJsonSerializerSettings = new JsonSerializerSettings
         {
             Converters = new List<JsonConverter> { new StringLongConverter() },
-            // 其他设置...
+            // Other settings...
         };
 
+        /// <summary>
+        /// Serializes an object to a JSON string.
+        /// </summary>
+        /// <param name="obj">The object to serialize.</param>
+        /// <returns>The JSON string representation of the object.</returns>
         public static string SerializeObject(object obj)
         {
             return JsonConvert.SerializeObject(obj, DefaultJsonSerializerSettings);
         }
     }
 
+    /// <summary>
+    /// Converts a string or integer to a long value during JSON serialization and deserialization.
+    /// </summary>
     public class StringLongConverter : JsonConverter
     {
+        /// <summary>
+        /// Determines whether this converter can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">The type of the object to convert.</param>
+        /// <returns>true if the converter can convert the specified type; otherwise, false.</returns>
         public override bool CanConvert(Type objectType)
         {
             return objectType == typeof(long);
         }
 
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The JSON writer.</param>
+        /// <param name="value">The value to write.</param>
+        /// <param name="serializer">The JSON serializer.</param>
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             writer.WriteValue(value.ToString());
         }
 
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The JSON reader.</param>
+        /// <param name="objectType">The type of the object to convert.</param>
+        /// <param name="existingValue">The existing value of the object being read.</param>
+        /// <param name="serializer">The JSON serializer.</param>
+        /// <returns>The deserialized object.</returns>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             if (reader.TokenType == JsonToken.String)
             {
-                // 如果 JSON 中的值是字符串，将其解析为 long 类型
+                // Parse the value as a long if it is a string in the JSON
                 if (long.TryParse(reader.Value?.ToString(), out long result))
                 {
                     return result;
@@ -49,7 +79,7 @@ namespace ReZero.SuperAPI
             }
             else if (reader.TokenType == JsonToken.Integer)
             {
-                // 如果 JSON 中的值是整数，直接转换为 long 类型
+                // Convert the value directly to long if it is an integer in the JSON
                 return Convert.ToInt64(reader.Value);
             }
             else
