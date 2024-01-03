29af8cf57fad610d5ad255e1c52d6823d41d0ceb
sunkaixuan <610262374@qq.com>
2024-01-03 14:14:17 +0800

    优化代码

--- ReZero/SuperAPI/Utils/Extensions.cs
+++ ReZero/SuperAPI/Utils/Extensions.cs
@@ -6,7 +6,13 @@ namespace ReZero
 {
     public static  class Extensions
     {
-        public static bool EqualsCase(this string a, string b) 
+        /// <summary>
+        /// Determines whether two strings are equal, ignoring case.
+        /// </summary>
+        /// <param name="a">The first string to compare.</param>
+        /// <param name="b">The second string to compare.</param>
+        /// <returns>True if the strings are equal, ignoring case; otherwise, false.</returns>
+        public static bool EqualsCase(this string a, string b)
         {
             return a?.ToLower() == b?.ToLower();
         }
