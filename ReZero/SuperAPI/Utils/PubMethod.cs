a7706e82afc6a6593a8c36c5dd9b3d97a5020eff
sunkaixuan <610262374@qq.com>
2024-01-03 14:16:44 +0800

    优化代码

--- ReZero/SuperAPI/Utils/PubMethod.cs
+++ ReZero/SuperAPI/Utils/PubMethod.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 namespace ReZero.SuperAPI
 {
     /// <summary>
-    /// 
+    /// Provides utility methods for common operations.
     /// </summary>
     internal class PubMethod
     {
@@ -17,7 +17,7 @@ namespace ReZero.SuperAPI
         /// <returns>The types derived from the base type</returns>
         public static List<Type> GetTypesDerivedFromDbBase(Type baseType)
         {
-            Assembly assembly= baseType.Assembly;
+            Assembly assembly = baseType.Assembly;
             List<Type> derivedTypes = new List<Type>();
             Type[] types = assembly.GetTypes();
             foreach (Type type in types)
@@ -30,7 +30,11 @@ namespace ReZero.SuperAPI
             return derivedTypes;
         }
 
-
+        /// <summary>
+        /// Checks if the given string is a valid property name.
+        /// </summary>
+        /// <param name="str">The string to check</param>
+        /// <returns>True if the string is a valid property name, otherwise false</returns>
         public static bool CheckIsPropertyName(string str)
         {
             return Regex.IsMatch(str, @"^[a-z,A-Z,_]\w*$");
