8d84d353afc90f334b951f5ee7e086bd20b0fb6e
sunkaixuan <610262374@qq.com>
2024-01-03 14:33:55 +0800

    -

--- ReZero/SuperAPI/Ui/IUiManager.cs
+++ ReZero/SuperAPI/Ui/IUiManager.cs
@@ -4,11 +4,24 @@ using System.Threading.Tasks;
 namespace ReZero.SuperAPI
 {
     /// <summary>
-    /// 默认UI使用，如果是Vue前后分离不会使用该类
+    /// Default UI usage, not applicable for Vue front-end and back-end separation
     /// </summary>
     public interface IUiManager
     {
+        /// <summary>
+        /// Retrieves the HTML content asynchronously.
+        /// </summary>
+        /// <param name="fileContent">The content of the file.</param>
+        /// <param name="filePath">The path of the file.</param>
+        /// <param name="content">The HTTP context.</param>
+        /// <returns>The HTML content as a string.</returns>
         Task<string> GetHtmlAsync(string fileContent, string filePath, Microsoft.AspNetCore.Http.HttpContext content);
+
+        /// <summary>
+        /// Checks if the file content represents a master page.
+        /// </summary>
+        /// <param name="fileContent">The content of the file.</param>
+        /// <returns>True if the file content represents a master page, otherwise false.</returns>
         bool IsMasterPage(string fileContent);
     }
 }
