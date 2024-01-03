0c1ed8df79f1e615034bcfc727dd4050c85eecd9
sunkaixuan <610262374@qq.com>
2024-01-03 14:20:15 +0800

    -

--- ReZero/SuperAPI/TextHandler/Enum/Language.cs
+++ ReZero/SuperAPI/TextHandler/Enum/Language.cs
@@ -4,9 +4,19 @@ using System.Text;
 
 namespace ReZero.SuperAPI
 {
+    /// <summary>
+    /// Represents the language options.
+    /// </summary>
     public enum Language
     {
+        /// <summary>
+        /// Chinese language.
+        /// </summary>
         CN,
+
+        /// <summary>
+        /// English language.
+        /// </summary>
         EN
     }
 }
