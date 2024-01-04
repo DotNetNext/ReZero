9748c361bb3b985c2e526a765ece98b392897d45
sunkaixuan <610262374@qq.com>
2024-01-04 13:33:02 +0800

    支持分组查询

---
+++ ReZero/SuperAPI/DataService/Query/QueryCommon/QueryCommon_GroupBy.cs
@@ -0,0 +1,30 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReZero.SuperAPI 
+{
+    /// <summary>
+    /// OrdeBy
+    /// </summary>
+    public partial class QueryCommon : IDataService
+    {
+        private QueryMethodInfo GroupName(Type type, DataModel dataModel, QueryMethodInfo queryObject)
+        {
+            List<GroupByModel> orderByModels = new List<GroupByModel>();
+            if (dataModel.OrderParemters != null)
+            {
+                foreach (var item in dataModel.OrderParemters)
+                {
+                    orderByModels.Add(new GroupByModel()
+                    {
+                        FieldName = GetFieldName(queryObject, item) 
+                    });
+                }
+            }
+            queryObject = queryObject.GroupBy(orderByModels);
+            return queryObject;
+        } 
+    }
+}
