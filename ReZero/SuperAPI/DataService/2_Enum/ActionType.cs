ba34182ac3924d7150fa8cb2273a0359557733f6
sunkaixuan <610262374@qq.com>
2024-01-07 20:16:26 +0800

    给枚举添加特性

--- ReZero/SuperAPI/DataService/2_Enum/ActionType.cs
+++ ReZero/SuperAPI/DataService/2_Enum/ActionType.cs
@@ -6,17 +6,38 @@ namespace ReZero.SuperAPI
 {
     public enum ActionType
     {
-        //Query
-        QueryByPrimaryKey=10000,
+         
+        #region Query
+        [ChineseText("根据主键查询")]
+        [EnglishText("Query by primary key")]
+        QueryByPrimaryKey = 10000,
+
+        [ChineseText("通用查询")]
+        [EnglishText("Common query")]
         QueryCommon = 10001,
+
+        [ChineseText("树型查询")]
+        [EnglishText("Common tree")]
         QueryTree = 10002,
+        #endregion
 
-        //Insert
-        InsertObject =20000,
 
-        //Delete
-        DeleteObject = 30000,
-        BizDeleteObject = 30001,
+        #region Insert
+        [ChineseText("插入根据实体")]
+        [EnglishText("Insert by entity")]
+        InsertObject = 20000,
+        #endregion
+
+
+        #region Delete
+        [ChineseText("删除根据实体")]
+        [EnglishText("Delete by entity")]
+        DeleteObject = 30000,  
+
+        [ChineseText("逻辑删除根据实体")]
+        [EnglishText("biz Delete by entity")]
+        BizDeleteObject = 30001, 
+        #endregion
 
         //Update
         UpdateObject = 40000,
