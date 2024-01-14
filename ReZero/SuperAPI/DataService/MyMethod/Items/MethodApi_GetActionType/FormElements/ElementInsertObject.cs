37625ee00a117df6f2a28d1837628c071213f6f0
sunkaixuan <610262374@qq.com>
2024-01-14 14:58:49 +0800

    添加配置

---
+++ ReZero/SuperAPI/DataService/MyMethod/Items/MethodApi_GetActionType/FormElements/ElementInsertObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using ReZero.SuperAPI;
+
+namespace ReZero.SuperAPI
+{
+    public class ElementInsertObject : BaseElement,IEelementActionType
+    {
+        public List<ActionTypeFormElementModel> GetModels()
+        {
+            var result = new List<ActionTypeFormElementModel>();
+            AddInterfaceName(result);
+            AddInterfacUrl(result);
+            AddGroup(result);
+            AddTable(result);
+            return result;
+        } 
+    }
+}
