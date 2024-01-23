f75eee3d5e8c72dac0c7b383a9ad3e7d8b02699f
sunkaixuan <610262374@qq.com>
2024-01-23 22:09:54 +0800

    接口管理

--- ReZero/SuperAPI/DataService/MyMethod/Items/MethodApi_GetActionType/DynamicElement/ActionTypeFormElementModel.cs
+++ ReZero/SuperAPI/DataService/MyMethod/Items/MethodApi_GetActionType/DynamicElement/ActionTypeFormElementModel.cs
@@ -9,6 +9,7 @@ namespace ReZero.SuperAPI
         public string? Name { get; set; }
         public string? Text { get; set; }
         public ElementType? ElementType { get; set; }
+        public string? Value { get; set; }
         public bool IsRequired { get; set; }
         public List<ActionTypeFormElementModel>? Items { get; set; }
 
