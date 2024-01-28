d57a75745da82fd99332243a943d808aa338c90d
sunkaixuan <610262374@qq.com>
2024-01-29 01:11:19 +0800

    -

--- ReZero/SuperAPI/DataService/MyMethod/Items/MethodApi_SaveInterfaceList/Items/SaveInterfaceListInsertObject.cs
+++ ReZero/SuperAPI/DataService/MyMethod/Items/MethodApi_SaveInterfaceList/Items/SaveInterfaceListInsertObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
 
@@ -11,7 +12,24 @@ namespace ReZero.SuperAPI
         {
             ZeroInterfaceList zeroInterfaceList = new ZeroInterfaceList();
             base.SetCommonProperties(zeroInterfaceList, saveInterfaceListModel);
+            this.SetProperties(zeroInterfaceList,saveInterfaceListModel);
             return base.InsertData(zeroInterfaceList);
+        }
+        private void SetProperties(ZeroInterfaceList zeroInterfaceList, SaveInterfaceListModel saveInterfaceListModel)
+        {
+            var entityInfo = base.GetEntityInfo(zeroInterfaceList!.DataModel!.TableId!);
+            zeroInterfaceList.DataModel.DefaultParameters = new List<DataModelDefaultParameter>();
+            foreach (var item in entityInfo.Columns.Where(it=>it.IsIdentity==false&&it.IsOnlyIgnoreInsert==false))
+            {
+                zeroInterfaceList.DataModel.DefaultParameters.Add(new DataModelDefaultParameter()
+                {
+                    FieldOperator = FieldOperatorType.Equal,
+                    Name = item.PropertyName,
+                    ParameterValidate =item.IsNullable?null:new ParameterValidate() { IsRequired = true },
+                    Description = item.ColumnDescription,
+                    ValueType = item.UnderType.Name
+                });
+            }
         }
     }
 }
