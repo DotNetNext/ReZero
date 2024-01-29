0f8ac6ca1fb68d30a4be66b7da8788cbd4178246
sunkaixuan <610262374@qq.com>
2024-01-29 10:51:18 +0800

    -

--- ReZero/SuperAPI/DataService/MyMethod/Items/MethodApi_GetActionType/FormElements/ElementQueryCommon.cs
+++ ReZero/SuperAPI/DataService/MyMethod/Items/MethodApi_GetActionType/FormElements/ElementQueryCommon.cs
@@ -13,7 +13,48 @@ namespace ReZero.SuperAPI
             var result = new List<ActionTypeFormElementModel>();
             base.AddActionTypeFormElementModels(result);
             base.AddActionTypeElementModel(result, this);
+            this.AddElements(result);
             return result;
-        } 
+        }
+
+        private void AddElements(List<ActionTypeFormElementModel> result)
+        {
+
+            result.Add(new ActionTypeFormElementModel()
+            {
+                ElementType = ElementType.Text,
+                Name = "Join",
+                Text = TextHandler.GetCommonText("Join json", "Join json"),
+                Value = null
+            });
+            result.Add(new ActionTypeFormElementModel()
+            {
+                ElementType = ElementType.Text,
+                Name = "Where",
+                Text=TextHandler.GetCommonText("Where json", "Where json"),
+                Value = null
+            });
+            result.Add(new ActionTypeFormElementModel()
+            {
+                ElementType = ElementType.Text,
+                Name = "OrderBy",
+                Text = TextHandler.GetCommonText("OrderBy json", "OrderBy json"),
+                Value = null
+            });
+            result.Add(new ActionTypeFormElementModel()
+            {
+                ElementType = ElementType.Text,
+                Name = "Select",
+                Text = TextHandler.GetCommonText("Select json", "Select json"),
+                Value = null
+            });
+            result.Add(new ActionTypeFormElementModel()
+            {
+                ElementType = ElementType.Text,
+                Name = nameof(DataModel.CommonPage.PageSize),
+                Text = TextHandler.GetCommonText("每页显示条数，不填不分页", "Page size"),
+                Value =null
+            }); 
+        }
     }
 }
