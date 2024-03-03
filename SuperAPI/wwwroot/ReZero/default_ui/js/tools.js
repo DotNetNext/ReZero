149ebc5892eb66d244798a39d8bf47f1fd19b6f9
sunkaixuan <610262374@qq.com>
2024-03-03 15:12:33 +0800

    -

--- SuperAPI/wwwroot/ReZero/default_ui/js/tools.js
+++ SuperAPI/wwwroot/ReZero/default_ui/js/tools.js
@@ -64,3 +64,10 @@
     }
 
 }
+Array.prototype.removeArrayItem = function (item) {
+    const index = this.indexOf(item);
+    if (index !== -1) {
+        this.splice(index, 1);
+    }
+    return this;
+};  
