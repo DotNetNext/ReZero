de15a0b473e3d4eba690f28ade1c01e6dc8ab392
sunkaixuan <610262374@qq.com>
2024-03-13 22:32:27 +0800

    生成固定排序

--- ReZero/SuperAPI/DataService/Query/QueryCommon/QueryCommon_OrderBy.cs
+++ ReZero/SuperAPI/DataService/Query/QueryCommon/QueryCommon_OrderBy.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ReZero.SuperAPI 
+namespace ReZero.SuperAPI
 {
     /// <summary>
     /// OrdeBy
@@ -13,6 +13,22 @@ namespace ReZero.SuperAPI
         private QueryMethodInfo OrderBy(Type type, DataModel dataModel, QueryMethodInfo queryObject)
         {
             List<OrderByModel> orderByModels = new List<OrderByModel>();
+            if (dataModel.OrderParemters != null)
+            {
+                foreach (var item in dataModel.OrderParemters)
+                {
+                    orderByModels.Add(new OrderByModel()
+                    {
+                        FieldName = GetFieldName(queryObject, new DataModelDynamicOrderParemter()
+                        {
+                            FieldName = item.FieldName,
+                            OrderByType = item.OrderByType,
+                            TableIndex = item.TableIndex
+                        }),
+                        OrderByType = item.OrderByType
+                    });
+                }
+            }
             if (dataModel.OrderDynamicParemters != null)
             {
                 foreach (var item in dataModel.OrderDynamicParemters)
@@ -30,7 +46,7 @@ namespace ReZero.SuperAPI
 
         private static string GetFieldName(QueryMethodInfo queryObject, DataModelDynamicOrderParemter item)
         {
-            var name= App.Db.EntityMaintenance.GetDbColumnName(item.FieldName, queryObject.EntityType);
+            var name = App.Db.EntityMaintenance.GetDbColumnName(item.FieldName, queryObject.EntityType);
             return PubConst.Orm_TableDefaultPreName + item.TableIndex + "." + name;
         }
     }
