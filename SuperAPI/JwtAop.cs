8516a34fa3f2c8cf81c5cec42dcfcd4d7e0008c0
sunkaixuan <610262374@qq.com>
2024-04-16 09:29:38 +0800

    -

---
+++ SuperAPI/JwtAop.cs
@@ -0,0 +1,20 @@
+﻿using ReZero.SuperAPI;
+
+namespace SuperAPITest
+{
+    public class JwtAop : DefaultSuperApiAop
+    {
+        public override Task OnExecutingAsync(InterfaceContext context)
+        { 
+            return base.OnExecutingAsync(context);
+        }
+        public override Task OnExecutedAsync(InterfaceContext context)
+        {
+            return base.OnExecutedAsync(context);
+        }
+        public override Task OnErrorAsync(InterfaceContext context)
+        {
+            return base.OnErrorAsync(context);
+        }
+    }
+}
