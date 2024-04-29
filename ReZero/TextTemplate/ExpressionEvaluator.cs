db19c24e4ec4f45887c7c6b336ed2e27f77cd513
sunkaixuan <610262374@qq.com>
2024-04-29 09:21:28 +0800

    -

---
+++ ReZero/TextTemplate/ExpressionEvaluator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Dynamic.Core;
+using System.Text;
+
+namespace ReZero.TextTemplate
+{
+    public class ExpressionEvaluator
+    {
+        public object Evaluate(string expression)
+        {
+            return DynamicExpressionParser.ParseLambda(new ParsingConfig() { }, typeof(object), expression, new object[] { }).Compile().DynamicInvoke();
+        }
+    }
+}
