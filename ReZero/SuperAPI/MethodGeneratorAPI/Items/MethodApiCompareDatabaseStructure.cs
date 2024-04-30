df12bd31779c6d90db4a188596d36830f85eafc8
sunkaixuan <610262374@qq.com>
2024-04-30 16:39:11 +0800

    优化结构对比错误

--- ReZero/SuperAPI/MethodGeneratorAPI/Items/MethodApiCompareDatabaseStructure.cs
+++ ReZero/SuperAPI/MethodGeneratorAPI/Items/MethodApiCompareDatabaseStructure.cs
@@ -10,38 +10,45 @@ namespace ReZero.SuperAPI
     { 
         public object CompareDatabaseStructure(List<string> ids)
         {
-            List<string> tableDifferences = new List<string>();
-            var result = string.Empty; 
-            var dbRoot = App.Db;
-            var entities=dbRoot.Queryable<ZeroEntityInfo>().In(ids.Select(it=>Convert.ToInt64(it)).ToList()).ToList(); 
-            foreach (var entity in entities)
+            try
             {
-                var codeFirstDb =App.GetDbTableId(entity.Id)!;
-                var type = EntityGeneratorManager.GetTypeAsync(entity.Id).GetAwaiter().GetResult();
-                if (codeFirstDb.DbMaintenance.IsAnyTable(codeFirstDb.EntityMaintenance.GetTableName(type),false))
+                List<string> tableDifferences = new List<string>();
+                var result = string.Empty;
+                var dbRoot = App.Db;
+                var entities = dbRoot.Queryable<ZeroEntityInfo>().In(ids.Select(it => Convert.ToInt64(it)).ToList()).ToList();
+                foreach (var entity in entities)
                 {
-                    var diff = codeFirstDb.CodeFirst.SetStringDefaultLength(255).GetDifferenceTables(type).ToDiffString();
-                    if (diff != null && !diff.Contains("No change"))
+                    var codeFirstDb = App.GetDbTableId(entity.Id)!;
+                    var type = EntityGeneratorManager.GetTypeAsync(entity.Id).GetAwaiter().GetResult();
+                    if (codeFirstDb.DbMaintenance.IsAnyTable(codeFirstDb.EntityMaintenance.GetTableName(type), false))
                     {
-                        tableDifferences.Add(diff);
+                        var diff = codeFirstDb.CodeFirst.SetStringDefaultLength(255).GetDifferenceTables(type).ToDiffString();
+                        if (diff != null && !diff.Contains("No change"))
+                        {
+                            tableDifferences.Add(diff);
+                        }
                     }
                 }
+                if (tableDifferences.Count == 0)
+                {
+                    result = $"<span class='diff_bule diff_success'>{TextHandler.GetCommonText("此操作没有风险，可以继续！！", "This operation is not risky and can continue!!")}</span>";
+                }
+                else
+                {
+                    result = string.Join("", tableDifferences).Replace("\n", "<br>");
+                }
+                result = result.Replace("<br>----", "<h3 class='diff_h3'>");
+                result = result.Replace("----", "</h3>");
+                result = result.Replace("Table:", $"{TextHandler.GetCommonText("表名", "Table")}:");
+                result = result.Replace("Add column", $"<span class='diff_bule'>{TextHandler.GetCommonText("添加列", "Add column")}</span>");
+                result = result.Replace("Update column", $"<span class='diff_yellow'>{TextHandler.GetCommonText("更新列", "Update column")}</span>");
+                result = result.Replace("Delete column", $"<span class='diff_red'>{TextHandler.GetCommonText("删除列", "Delete column")}</span>");
+                return result;
             }
-            if (tableDifferences.Count == 0)
+            catch
             {
-                result = $"<span class='diff_bule diff_success'>{TextHandler.GetCommonText("此操作没有风险，可以继续！！", "This operation is not risky and can continue!!")}</span>";
+                return "结构对比出现错误，请谨慎同步";
             }
-            else
-            {
-                result = string.Join("", tableDifferences).Replace("\n", "<br>");
-            }
-            result = result.Replace("<br>----", "<h3 class='diff_h3'>");
-            result = result.Replace("----", "</h3>");
-            result = result.Replace("Table:", $"{TextHandler.GetCommonText("表名","Table")}:");
-            result = result.Replace("Add column", $"<span class='diff_bule'>{TextHandler.GetCommonText("添加列", "Add column")}</span>");
-            result = result.Replace("Update column", $"<span class='diff_yellow'>{TextHandler.GetCommonText("更新列", "Update column")}</span>");
-            result = result.Replace("Delete column", $"<span class='diff_red'>{TextHandler.GetCommonText("删除列", "Delete column")}</span>");
-            return result;
         }
     }
 }
