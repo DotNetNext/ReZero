3601cc580693ff94fe8844f8041d51ae17c40833
sunkaixuan <610262374@qq.com>
2024-05-04 22:39:16 +0800

    添加动态API

---
+++ ReZero/SuperAPI/ApiDynamic/Entities/DynamicMethodAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReZero.SuperAPI 
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class DynamicMethodAttribute : Attribute
+    {
+        private string url { get; set; }
+        private long InterfaceCategoryId { get; set; }
+        private string GroupName { get; set; }
+        public DynamicMethodAttribute(string url,long interfaceCategoryId, string groupName) 
+        {
+            this.url = url;
+            this.InterfaceCategoryId = interfaceCategoryId;
+            this.GroupName = groupName; 
+        }
+        public DynamicMethodAttribute(string url, long interfaceCategoryId)
+        {
+            this.url = url;
+            this.InterfaceCategoryId = interfaceCategoryId;
+            this.GroupName =string.Empty;
+        }
+        public DynamicMethodAttribute(string url)
+        {
+            this.url = url;
+            this.InterfaceCategoryId = 0;
+            this.GroupName = string.Empty;
+        }
+    }
+}
---
+++ SuperAPI/Controllers/MyApiController.cs
@@ -0,0 +1,14 @@
+﻿using ReZero.SuperAPI;
+using ReZero.SuperAPI.ApiDynamic.Entities;
+namespace SuperAPITest.Controllers
+{
+    [DynamicApi(21000)]
+    public class MyApiController
+    {
+        [DynamicMethod("/aa/yyy")]
+        public int A(int i) 
+        {
+            return 1;
+        }
+    }
+}
