5f095be0df9963b56beb202dcb1b7a9ae2968fac
sunkaixuan <610262374@qq.com>
2024-05-11 14:27:14 +0800

    树型查询功能

--- ReZero/SuperAPI/MethodGeneratorAPI/Items/MethodApiGetActionType/FormElements/ElementQueryTree.cs
+++ ReZero/SuperAPI/MethodGeneratorAPI/Items/MethodApiGetActionType/FormElements/ElementQueryTree.cs
@@ -13,6 +13,28 @@ namespace ReZero.SuperAPI
             var result = new List<ActionTypeFormElementModel>();
             base.AddActionTypeFormElementModels(result);
             base.AddActionTypeElementModel(result, this);
+            result.Insert(3, new ActionTypeFormElementModel()
+            {
+                 ElementType= ElementType.Text,
+                 Name="Id",
+                 Text=TextHandler.GetCommonText("编号字段名", "Number field name"),
+                 IsRequired=true,
+            });
+            result.Insert(4, new ActionTypeFormElementModel()
+            {
+                ElementType = ElementType.Text,
+                Name = "Id",
+                Text = TextHandler.GetCommonText("父级编号字段名", "Parent number Field name"),
+                IsRequired = true,
+            });
+            result.Insert(5, new ActionTypeFormElementModel()
+            {
+                ElementType = ElementType.Text,
+                Name = "Id",
+                Text = TextHandler.GetCommonText("根目录ParentId的值", "Primary key of the root directory"),
+                Placeholder="一般是0或者空，例如【Id=1,ParentId=0】，那么就是0",
+                IsRequired = true,
+            });
             return result;
         } 
     }
