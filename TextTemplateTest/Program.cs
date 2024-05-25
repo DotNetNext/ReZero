45823cd2b2f4aaf485d958f8170d1448ad8cdbdf
sunkaixuan <610262374@qq.com>
2024-05-25 15:11:07 +0800

    添加DEMO

--- TextTemplateTest/Program.cs
+++ TextTemplateTest/Program.cs
@@ -1,14 +1,37 @@
 ﻿using ReZero.TextTemplate;
 using System.Text;
+ 
+Console.WriteLine(Demo1());
+Console.WriteLine(Demo2());
+Console.WriteLine(Demo3());
+Console.ReadLine();
 
-/****************该功能还在开发中*****************/
-var x = new TextTemplateManager();
-var template = @"<div>{{condition.ToString().ToUpper()}}</div> 
+static string Demo1()
+{
+    var x = new TextTemplateManager();
+    var template = @"<div>{ {condition.ToString().ToUpper()}}</div> 
  <% foreach(var item in collection) {  %>
 <div>{{item}}</div>
 <%  } %>
 ";
-var data = new Model{ condition = true, collection = new[] { "Item 1", "Item 2", "Item 3" } };
-var str=x.RenderTemplate(template,data);
-Console.WriteLine(str); 
-Console.ReadLine();
+    var data = new Model { condition = true, collection = new[] { "Item 1", "Item 2", "Item 3" } };
+    var str = x.RenderTemplate(template, data);
+    return str;
+}
+static string Demo2()
+{
+    var x = new TextTemplateManager();
+    var template = @" { { condition } }";
+    var data = new Model { condition = true, collection = new[] { "Item 1", "Item 2", "Item 3" } };
+    var str = x.RenderTemplate(template, data);
+    return str;
+}
+static string Demo3()
+{
+    var x = new TextTemplateManager();
+    var template = @" < %  var id=1;  % >
+{{id}}+{{true}}";
+    var data = new Model { condition = true, collection = new[] { "Item 1", "Item 2", "Item 3" } };
+    var str = x.RenderTemplate(template, data);
+    return str;
+}
