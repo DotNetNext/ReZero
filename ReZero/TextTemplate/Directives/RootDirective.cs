0abfc58eae137e7e6adc0480056f80520187d14a
sunkaixuan <610262374@qq.com>
2024-05-25 15:06:04 +0800

    添加矫正

--- ReZero/TextTemplate/Directives/RootDirective.cs
+++ ReZero/TextTemplate/Directives/RootDirective.cs
@@ -11,9 +11,9 @@ namespace ReZero.TextTemplate
         public string Execute(string input, object data, ITemplateEngine templateEngine)
         {
             input = Regex.Replace(input, @"\{\ {1,5}\{", "{{");
-            input = Regex.Replace(input, @"\}[ ]{1,5}\}", "}}");
-            input = Regex.Replace(input, @"\<\ {1,5}\%", "{{");
-            input = Regex.Replace(input, @"\%[ ]{1,5}\>", "}}");
+            input = Regex.Replace(input, @"\} {1,5}\}", "}}");
+            input = Regex.Replace(input, @"\<\ {1,5}\%", "<%");
+            input = Regex.Replace(input, @"\% {1,5}\>", "%>");
 
             StringBuilder sb = new StringBuilder();
             sb.Append("string result = ");
