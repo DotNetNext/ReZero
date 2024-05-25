15e97515e9f520c7a1f4bc9e038059f19772165a
sunkaixuan <610262374@qq.com>
2024-05-25 11:40:37 +0800

    完成导出excel

--- ReZero/SuperAPI/MethodGeneratorAPI/Items/MethodApiExcel.cs
+++ ReZero/SuperAPI/MethodGeneratorAPI/Items/MethodApiExcel.cs
@@ -1,4 +1,4 @@
-﻿using SqlSugar;
+﻿ using SqlSugar;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -8,49 +8,50 @@ using System.Text;
 namespace ReZero.SuperAPI 
 {
     public partial class MethodApi
-    {  
-        public byte[] ExportEntities(long [] tableIds)
-        { 
+    {
+        public byte[] ExportEntities(long[] tableIds)
+        {
             List<DataTable> datatables = new List<DataTable>();
             var db = App.Db;
-            var datas=db.Queryable<ZeroEntityInfo>().WhereIF(tableIds.Any(), it=> tableIds.Contains(it.Id)).Includes(it => it.ZeroEntityColumnInfos).ToList();
+            var datas = db.Queryable<ZeroEntityInfo>().WhereIF(tableIds.Any(), it => tableIds.Contains(it.Id)).Includes(it => it.ZeroEntityColumnInfos).ToList();
             foreach (var item in datas)
             {
+                var columnInfos = db.DbMaintenance.GetColumnInfosByTableName(item.DbTableName, false);
                 DataTable dt = new DataTable();
-                dt.Columns.Add("列名");
-                dt.Columns.Add("列描述");
-                dt.Columns.Add("列类型");
-                dt.Columns.Add("实体类型");
-                dt.Columns.Add("主键");
-                dt.Columns.Add("自增");
-                dt.Columns.Add("可空");
-                dt.Columns.Add("长度");
-                dt.Columns.Add("精度");
-                dt.Columns.Add("默认值");
-                dt.Columns.Add("表名"); ;
-                dt.Columns.Add("表描述");
-                foreach (var it in item.ZeroEntityColumnInfos!)
+                dt.Columns.Add(TextHandler.GetCommonText("列名", "Field name"));
+                dt.Columns.Add(TextHandler.GetCommonText("列描述", "Column description"));
+                dt.Columns.Add(TextHandler.GetCommonText("列类型", "Column type"));
+                dt.Columns.Add(TextHandler.GetCommonText("实体类型", "Entity type"));
+                dt.Columns.Add(TextHandler.GetCommonText("主键", "Primary key"));
+                dt.Columns.Add(TextHandler.GetCommonText("自增", "Auto increment"));
+                dt.Columns.Add(TextHandler.GetCommonText("可空", "Nullable"));
+                dt.Columns.Add(TextHandler.GetCommonText("长度", "Length"));
+                dt.Columns.Add(TextHandler.GetCommonText("精度", "Precision"));
+                dt.Columns.Add(TextHandler.GetCommonText("默认值", "Default value"));
+                dt.Columns.Add(TextHandler.GetCommonText("表名", "Table name"));
+                dt.Columns.Add(TextHandler.GetCommonText("表描述", "Table description"));
+                foreach (var it in columnInfos!)
                 {
                     var dr = dt.NewRow();
-                    dr["列名"] = it.DbColumnName;
-                    dr["列描述"] = it.Description;
-                    dr["列类型"] = it.DataType;
-                    dr["实体类型"] = it.PropertyType;
-                    dr["表名"] = item.DbTableName;
-                    dr["表描述"] = item.Description;
+                    dr[TextHandler.GetCommonText("列名", "Field name")] = it.DbColumnName;
+                    dr[TextHandler.GetCommonText("列描述", "Column description")] = it.ColumnDescription;
+                    dr[TextHandler.GetCommonText("列类型", "Column type")] = it.DataType;
+                    dr[TextHandler.GetCommonText("实体类型", "Entity type")] = it.PropertyType;
+                    dr[TextHandler.GetCommonText("表名", "Table name")] = item.DbTableName;
+                    dr[TextHandler.GetCommonText("表描述", "Table description")] = item.Description;
 
-                    dr["主键"] = it.IsPrimarykey ? "yes" : "";
-                    dr["自增"] = it.IsIdentity ? "yes" : "";
-                    dr["可空"] = it.IsNullable ? "yes" : "";
-                    dr["长度"] = it.Length;
-                    dr["精度"] = it.DecimalDigits; 
+                    dr[TextHandler.GetCommonText("主键", "Primary key")] = it.IsPrimarykey ? "yes" : "";
+                    dr[TextHandler.GetCommonText("自增", "Auto increment")] = it.IsIdentity ? "yes" : "";
+                    dr[TextHandler.GetCommonText("可空", "Nullable")] = it.IsNullable ? "yes" : "";
+                    dr[TextHandler.GetCommonText("长度", "Length")] = it.Length;
+                    dr[TextHandler.GetCommonText("精度", "Precision")] = it.DecimalDigits;
 
                     dt.Rows.Add(dr);
                 }
                 dt.TableName = item.DbTableName;
                 datatables.Add(dt);
             }
-            return ReZero.Excel.DataTableToExcel.ExportExcel(datatables.ToArray(),$"{DateTime.Now.ToString("实体文档.xlsx")}");
+            return ReZero.Excel.DataTableToExcel.ExportExcel(datatables.ToArray(), $"{DateTime.Now.ToString("实体文档.xlsx")}");
         }
     }
 }
