4452a71599b3fe99fa8b1813262183eb9cbe2315
sunkaixuan <610262374@qq.com>
2024-05-28 20:24:02 +0800

    添加实体生成文件

---
+++ ReZero/SuperAPI/MethodGeneratorAPI/Entities/TemplateEntitiesGen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ReZero.SuperAPI 
+{
+    /// <summary>
+    /// 生成实体结构
+    /// </summary>
+    public class TemplateEntitiesGen
+    {  
+        /// <summary>
+        /// 类名
+        /// </summary>
+        public string? ClassName { get; set; }
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string? TableName { get; set; }
+        /// <summary>
+        /// 备注 
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// 列集合
+        /// </summary>
+        public List<TemplatePropertyGen>? PropertyGens { get; set; }
+
+    } 
+}
---
+++ ReZero/SuperAPI/MethodGeneratorAPI/Entities/TemplatePropertyGen.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ReZero.SuperAPI 
+{
+
+    /// <summary>
+    /// 属性和列
+    /// </summary>
+    public class TemplatePropertyGen
+    {
+        /// <summary>
+        /// 属性名
+        /// </summary>
+        public string? PropertyName { get; set; }
+        /// <summary>
+        /// 列名
+        /// </summary>
+        public string? DbColumnName { get; set; }
+
+        /// <summary>
+        /// 属性类型
+        /// </summary>
+        public string? PropertyType { get; set; }
+
+        /// <summary>
+        /// 数据库类型
+        /// </summary>
+        public string? DbType { get; set; }
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public bool IsPrimaryKey { get; set; }
+        /// <summary>
+        /// 自增列 
+        /// </summary>
+        public bool IsIdentity { get; set; }
+        /// <summary>
+        /// 备注 
+        /// </summary>
+        public string? Description { get; set; }
+        /// <summary>
+        /// 是否是为NULL
+        /// </summary>
+        public bool IsNullable { get; set; }
+        /// <summary>
+        /// Mapping精度
+        /// </summary>
+        public int? DecimalDigits { get; set; } 
+        /// <summary>
+        /// 是否忽略
+        /// </summary>
+        public bool IsIgnore { get; set; }
+        /// <summary>
+        /// 特殊类型
+        /// </summary>
+        public bool IsSpecialType { get; set; } 
+        /// <summary>
+        /// 默认值
+        /// </summary>
+        public string? DefaultValue { get; set; }   
+    }
+}
