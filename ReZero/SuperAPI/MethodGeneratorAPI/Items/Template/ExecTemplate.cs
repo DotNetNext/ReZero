6e769ec694033097d7fe542afa5bbf12f44eb121
sunkaixuan <610262374@qq.com>
2024-05-30 12:12:56 +0800

    代码生成

--- ReZero/SuperAPI/MethodGeneratorAPI/Items/Template/ExecTemplate.cs
+++ ReZero/SuperAPI/MethodGeneratorAPI/Items/Template/ExecTemplate.cs
@@ -1,4 +1,5 @@
-﻿using SqlSugar;
+﻿using ReZero.TextTemplate;
+using SqlSugar;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,26 @@ namespace ReZero.SuperAPI
 {
     public partial class MethodApi
     {
-         
+        public string ExecTemplate(TemplateType type, string templateJson,string resultJson) 
+        {
+            string result = string.Empty;
+            switch (type)
+            {
+                case TemplateType.Entity:
+                    result = ExecTemplateByEntity(templateJson,resultJson);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid template type.");
+            }
+
+            return result; 
+        }
+
+        private string ExecTemplateByEntity(string templateJson, string resultJson)
+        {
+            TemplateModel<TemplateEntitiesGen> model = new SerializeService().DeserializeObject<TemplateModel<TemplateEntitiesGen>>(resultJson);
+            var temp = new TextTemplateManager().RenderTemplate(templateJson, model);
+            return temp.ToString();
+        }
     }
 }
