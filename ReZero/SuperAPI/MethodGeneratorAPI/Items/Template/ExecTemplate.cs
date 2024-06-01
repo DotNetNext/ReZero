6de5f94b15490ae66f8ebd69ba32613e7c7e1458
sunkaixuan <610262374@qq.com>
2024-06-01 08:58:04 +0800

    添加代码生成

--- ReZero/SuperAPI/MethodGeneratorAPI/Items/Template/ExecTemplate.cs
+++ ReZero/SuperAPI/MethodGeneratorAPI/Items/Template/ExecTemplate.cs
@@ -1,13 +1,59 @@
-﻿using ReZero.TextTemplate;
+﻿using ReZero.Excel;
+using ReZero.TextTemplate;
 using SqlSugar;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ReZero.SuperAPI
 {
     public partial class MethodApi
     {
+        public string ExecTemplateByTableIds(long databaseId, long[] tableIds,long tempId) 
+        {
+            List<ExcelData> datatables = new List<ExcelData>();
+            var db = App.Db;
+            var datas = db.Queryable<ZeroEntityInfo>()
+                .OrderBy(it => it.DbTableName)
+                .Where(it => it.DataBaseId == databaseId)
+                .WhereIF(tableIds.Any(), it => tableIds.Contains(it.Id))
+                .Includes(it => it.ZeroEntityColumnInfos).ToList();
+            List<TemplateEntitiesGen> gens = new List<TemplateEntitiesGen>();
+            var template= App.Db.Queryable<ZeroTemplate>().First(it => it.Id == tempId);
+            foreach (var item in datas)
+            {
+                var propertyGens = new List<TemplatePropertyGen>();
+                TemplateEntitiesGen templateEntitiesGen = new TemplateEntitiesGen()
+                {
+                    ClassName = item.ClassName,
+                    Description = item.Description,
+                    TableName = item.DbTableName,
+                    PropertyGens = propertyGens
+                }; 
+                var columnInfos = App.GetDbById(databaseId)!.DbMaintenance.GetColumnInfosByTableName(item.DbTableName, false);
+                foreach (var columnInfo in columnInfos)
+                {
+                    var zeroEntityColumn = item.ZeroEntityColumnInfos.FirstOrDefault(it=>it.DbColumnName!.EqualsCase( columnInfo.DbColumnName));
+                    TemplatePropertyGen templatePropertyGen = new TemplatePropertyGen()
+                    {
+                         DbColumnName=columnInfo.DbColumnName,
+                         DbType= string.IsNullOrEmpty(columnInfo.OracleDataType)? columnInfo.DataType: columnInfo.OracleDataType,
+                         DecimalDigits=columnInfo.DecimalDigits,
+                         DefaultValue=columnInfo.DefaultValue,
+                         Description=columnInfo.ColumnDescription,
+                         IsIdentity=columnInfo.IsIdentity,
+                         IsNullable=columnInfo.IsNullable,
+                         IsPrimaryKey=columnInfo.IsPrimarykey, 
+                         PropertyName= zeroEntityColumn.PropertyName,
+                         PropertyType=EntityGeneratorManager.GetTypeByNativeTypes(zeroEntityColumn.PropertyType).Name+(zeroEntityColumn.IsNullable?"?":string.Empty),
+                         IsJson=zeroEntityColumn.IsJson 
+                    }; 
+                }
+            }
+            return ExecTemplate(TemplateType.Entity, new SerializeService().SerializeObject(gens), template.TemplateContent!);
+        }
+
         public string ExecTemplate(TemplateType type, string data, string template) 
         {
             string result = string.Empty;
