42441ed63378ed2c0e572b613b2e67af6c960280
sunkaixuan <610262374@qq.com>
2024-06-01 15:28:30 +0800

    优化代码

--- ReZero/SuperAPI/MethodGeneratorAPI/Items/Template/ExecTemplate.cs
+++ ReZero/SuperAPI/MethodGeneratorAPI/Items/Template/ExecTemplate.cs
@@ -15,7 +15,15 @@ namespace ReZero.SuperAPI
     public partial class MethodApi
     {
         #region ExecTemplateByTableIds
-        public string ExecTemplateByTableIds(long databaseId, long[] tableIds, long templateId,string url)
+        /// <summary>
+        /// Execute template generation for multiple tables.
+        /// </summary>
+        /// <param name="databaseId">The ID of the database.</param>
+        /// <param name="tableIds">The IDs of the tables.</param>
+        /// <param name="templateId">The ID of the template.</param>
+        /// <param name="url">The URL of the output file.</param>
+        /// <returns>The directory path of the generated files.</returns>
+        public string ExecTemplateByTableIds(long databaseId, long[] tableIds, long templateId, string url)
         {
             List<ExcelData> datatables = new List<ExcelData>();
             var db = App.Db;
@@ -24,34 +32,51 @@ namespace ReZero.SuperAPI
             var outUrl = string.Empty;
             foreach (var item in datas)
             {
-                outUrl=CreateFile(databaseId, template, item, url);
+                outUrl = CreateFile(databaseId, template, item, url);
             }
             var result = Directory.GetParent(outUrl).FullName;
             return result;
         }
-        private string CreateFile(long databaseId, ZeroTemplate template, ZeroEntityInfo item,string url)
-        { 
-            var  classString = GetClassString(databaseId, template, item,out TemplateEntitiesGen templateEntitiesGen);
-            url = GetUrl(url,templateEntitiesGen);
-            if (url.Contains(PubConst.Common_Project)) 
-            { 
+
+        /// <summary>
+        /// Create a file based on the template and entity information.
+        /// </summary>
+        /// <param name="databaseId">The ID of the database.</param>
+        /// <param name="template">The template to use.</param>
+        /// <param name="item">The entity information.</param>
+        /// <param name="url">The URL of the output file.</param>
+        /// <returns>The URL of the created file.</returns>
+        private string CreateFile(long databaseId, ZeroTemplate template, ZeroEntityInfo item, string url)
+        {
+            var classString = GetClassString(databaseId, template, item, out TemplateEntitiesGen templateEntitiesGen);
+            url = GetUrl(url, templateEntitiesGen);
+            if (url.Contains(PubConst.Common_Project))
+            {
                 var baseDir = AppContext.BaseDirectory;
                 var findDir = DirectoryHelper.FindParentDirectoryWithSlnFile(baseDir);
                 if (!string.IsNullOrEmpty(findDir))
                 {
-                    url= Regex.Replace(url,PubConst.Common_ProjectRegex,string.Empty,RegexOptions.IgnoreCase).TrimStart('/').TrimStart('\\');
-                    url = Path.Combine(findDir,url);
+                    url = Regex.Replace(url, PubConst.Common_ProjectRegex, string.Empty, RegexOptions.IgnoreCase).TrimStart('/').TrimStart('\\');
+                    url = Path.Combine(findDir, url);
                 }
-                else 
+                else
                 {
                     throw new Exception(TextHandler.GetCommonText("没有找到 项目sln文件,可以使完整物理路径", "No project sln file found that can make the full physical path"));
                 }
             }
-            FileSugar.CreateFileReplace(url, classString,Encoding.UTF8);
+            FileSugar.CreateFileReplace(url, classString, Encoding.UTF8);
             return url;
         }
 
-        private string GetClassString(long databaseId, ZeroTemplate template, ZeroEntityInfo item,out TemplateEntitiesGen templateEntitiesGen)
+        /// <summary>
+        /// Get the class string based on the template and entity information.
+        /// </summary>
+        /// <param name="databaseId">The ID of the database.</param>
+        /// <param name="template">The template to use.</param>
+        /// <param name="item">The entity information.</param>
+        /// <param name="templateEntitiesGen">The generated template entities.</param>
+        /// <returns>The class string.</returns>
+        private string GetClassString(long databaseId, ZeroTemplate template, ZeroEntityInfo item, out TemplateEntitiesGen templateEntitiesGen)
         {
             var propertyGens = new List<TemplatePropertyGen>();
             templateEntitiesGen = new TemplateEntitiesGen()
@@ -69,6 +94,13 @@ namespace ReZero.SuperAPI
             var classString = ExecTemplate(TemplateType.Entity, new SerializeService().SerializeObject(templateEntitiesGen), template.TemplateContent!);
             return classString;
         }
+
+        /// <summary>
+        /// Add a property to the property list based on the entity column information.
+        /// </summary>
+        /// <param name="propertyGens">The list of property generators.</param>
+        /// <param name="columnInfos">The list of column information.</param>
+        /// <param name="zeroEntityColumn">The entity column information.</param>
         private static void AddProperty(List<TemplatePropertyGen> propertyGens, List<SqlSugar.DbColumnInfo> columnInfos, ZeroEntityColumnInfo zeroEntityColumn)
         {
             var dbColumn = columnInfos.FirstOrDefault(it => it.DbColumnName!.EqualsCase(zeroEntityColumn.DbColumnName!));
@@ -92,6 +124,11 @@ namespace ReZero.SuperAPI
             propertyGens.Add(templatePropertyGen);
         }
 
+        /// <summary>
+        /// Process the property based on the database column information.
+        /// </summary>
+        /// <param name="dbColumn">The database column information.</param>
+        /// <param name="templatePropertyGen">The template property generator.</param>
         private static void ProcessingPropertyByDbColumn(SqlSugar.DbColumnInfo dbColumn, TemplatePropertyGen templatePropertyGen)
         {
             if (dbColumn != null)
@@ -101,19 +138,38 @@ namespace ReZero.SuperAPI
                 templatePropertyGen.Length = dbColumn.Length;
                 templatePropertyGen.IsNullable = dbColumn.IsNullable;
             }
-        } 
+        }
+
+        /// <summary>
+        /// Process the default value of the property.
+        /// </summary>
+        /// <param name="zeroEntityColumn">The entity column information.</param>
+        /// <param name="templatePropertyGen">The template property generator.</param>
         private static void ProcessingPropertyDefault(ZeroEntityColumnInfo zeroEntityColumn, TemplatePropertyGen templatePropertyGen)
         {
             templatePropertyGen.PropertyType = EntityGeneratorManager.GetNativeTypeName(templatePropertyGen.PropertyType!);
             templatePropertyGen.PropertyType = templatePropertyGen.PropertyType + (zeroEntityColumn.IsNullable ? PubConst.Common_Q : string.Empty);
         }
 
-
+        /// <summary>
+        /// Get the URL based on the template entities information.
+        /// </summary>
+        /// <param name="url">The URL template.</param>
+        /// <param name="templateEntitiesGen">The generated template entities.</param>
+        /// <returns>The URL with replaced placeholders.</returns>
         private static string GetUrl(string url, TemplateEntitiesGen templateEntitiesGen)
         {
             url = url.Replace(PubConst.Common_Format0, templateEntitiesGen.ClassName).Replace(PubConst.Common_Format1, templateEntitiesGen.TableName);
             return url;
-        } 
+        }
+
+        /// <summary>
+        /// Get the entity information for the specified database and table IDs.
+        /// </summary>
+        /// <param name="databaseId">The ID of the database.</param>
+        /// <param name="tableIds">The IDs of the tables.</param>
+        /// <param name="db">The SQL Sugar client.</param>
+        /// <returns>The list of entity information.</returns>
         private static List<ZeroEntityInfo> GetZeroEntities(long databaseId, long[] tableIds, ISqlSugarClient db)
         {
             return db.Queryable<ZeroEntityInfo>()
@@ -122,9 +178,16 @@ namespace ReZero.SuperAPI
                 .WhereIF(tableIds.Any(), it => tableIds.Contains(it.Id))
                 .Includes(it => it.ZeroEntityColumnInfos).ToList();
         }
-        #endregion 
+        #endregion
 
         #region ExecTemplate
+        /// <summary>
+        /// Execute template generation based on the specified type, data, and template.
+        /// </summary>
+        /// <param name="type">The type of the template.</param>
+        /// <param name="data">The data to use for template generation.</param>
+        /// <param name="template">The template to use.</param>
+        /// <returns>The generated template result.</returns>
         public string ExecTemplate(TemplateType type, string data, string template)
         {
             string result = string.Empty;
@@ -140,13 +203,19 @@ namespace ReZero.SuperAPI
             return result;
         }
 
+        /// <summary>
+        /// Execute template generation based on the entity type.
+        /// </summary>
+        /// <param name="data">The data to use for template generation.</param>
+        /// <param name="template">The template to use.</param>
+        /// <returns>The generated template result.</returns>
         private string ExecTemplateByEntity(string data, string template)
         {
             var model = new SerializeService().DeserializeObject<TemplateEntitiesGen>(data);
             var templateModel = new TemplateModel<TemplateEntitiesGen> { Model = model };
             var temp = new TextTemplateManager().RenderTemplate(template, templateModel);
             return temp.ToString();
-        } 
+        }
         #endregion
     }
 }
