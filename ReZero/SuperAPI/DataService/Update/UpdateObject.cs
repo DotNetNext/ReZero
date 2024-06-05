a1cc58bfe04a45bfc5b81a8c1b0e4c30e09fdea7
sunkaixuan <610262374@qq.com>
2024-06-05 12:37:39 +0800

    优化代码

--- ReZero/SuperAPI/DataService/Update/UpdateObject.cs
+++ ReZero/SuperAPI/DataService/Update/UpdateObject.cs
@@ -10,7 +10,7 @@ namespace ReZero.SuperAPI
     internal class UpdateObject : CommonDataService, IDataService
     {
         public async Task<object> ExecuteAction(DataModel dataModel)
-        { ;
+        {  
             var db = App.GetDbTableId(dataModel.TableId) ?? App.Db;
             var type = await EntityGeneratorManager.GetTypeAsync(dataModel.TableId);
             base.InitDb(type, db);
