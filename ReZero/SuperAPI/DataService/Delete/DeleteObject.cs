e18683cdd967624d53c3fe17fa2b57dcc0c67b8c
sunkaixuan <610262374@qq.com>
2024-06-05 12:28:26 +0800

    缓存管理

--- ReZero/SuperAPI/DataService/Delete/DeleteObject.cs
+++ ReZero/SuperAPI/DataService/Delete/DeleteObject.cs
@@ -10,6 +10,8 @@ namespace ReZero.SuperAPI
         public async Task<object> ExecuteAction(DataModel dataModel)
         {
             base.ClearZeroInterfaceListCache(dataModel);
+            base.ClearZeroEntityInfoInfoCache(dataModel);
+            base.ClearZeroDatabaseInfoCache(dataModel);
             var db = App.GetDbTableId(dataModel.TableId) ?? App.Db;
             var type = await EntityGeneratorManager.GetTypeAsync(dataModel.TableId);
             base.InitDb(type, db);
