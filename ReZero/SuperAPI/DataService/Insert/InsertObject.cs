d3534e97e38ac2a06c28f6edf2962b6501736f18
sunkaixuan <610262374@qq.com>
2024-06-05 12:26:20 +0800

    缓存管理

--- ReZero/SuperAPI/DataService/Insert/InsertObject.cs
+++ ReZero/SuperAPI/DataService/Insert/InsertObject.cs
@@ -12,6 +12,7 @@ namespace ReZero.SuperAPI
         public async Task<object> ExecuteAction(DataModel dataModel)
         {
             base.ClearZeroDatabaseInfoCache(dataModel);
+            base.ClearZeroEntityInfoInfoCache(dataModel);
             var db = App.GetDbTableId(dataModel.TableId) ?? App.Db;
             var type = await EntityGeneratorManager.GetTypeAsync(dataModel.TableId);
             base.InitDb(type, db);
