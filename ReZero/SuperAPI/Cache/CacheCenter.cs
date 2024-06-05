43387f06c86dd708d399a994a0157db2fc493dd2
sunkaixuan <610262374@qq.com>
2024-06-05 12:18:04 +0800

    添加清除所有缓存

--- ReZero/SuperAPI/Cache/CacheCenter.cs
+++ ReZero/SuperAPI/Cache/CacheCenter.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace ReZero.SuperAPI 
 {
-    internal class CacheCenter
+    public class CacheCenter
     {
+        private static readonly object cacheLock = new object();
+        public void ClearAllInternalCache() 
+        {
+            lock (cacheLock)
+            {
+                CacheManager<ZeroDatabaseInfo>.Instance.ClearCache();
+                CacheManager<ZeroInterfaceList>.Instance.ClearCache();
+                CacheManager<ZeroEntityInfo>.Instance.ClearCache();
+            }
+        }
     }
 }
