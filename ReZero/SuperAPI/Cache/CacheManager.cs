5c30b49d28e28851ea53315630ac0f00bf1a2808
sunkaixuan <610262374@qq.com>
2024-06-05 13:11:24 +0800

    优化代码

--- ReZero/SuperAPI/Cache/CacheManager.cs
+++ ReZero/SuperAPI/Cache/CacheManager.cs
@@ -6,7 +6,7 @@ namespace ReZero.SuperAPI
 {
     public class  CacheManager<T>: ICacheManager<T> where T:class,new()
     {
-        private static List<T> cacheZeroDatabaseInfo = null!;
+        private static List<T> cacheObject = null!;
         private static readonly object cacheLock = new object();
 
         public static CacheManager<T> Instance
@@ -18,28 +18,28 @@ namespace ReZero.SuperAPI
         }
         public List<T> GetList()
         {
-            if (cacheZeroDatabaseInfo != null)
+            if (cacheObject != null)
             {
-                return cacheZeroDatabaseInfo;
+                return cacheObject;
             }
 
             lock (cacheLock)
             {
-                if (cacheZeroDatabaseInfo == null)
+                if (cacheObject == null)
                 {
                     var db = App.Db;
-                    cacheZeroDatabaseInfo = db.Queryable<T>().ToList();
+                    cacheObject = db.Queryable<T>().ToList();
                 }
             }
 
-            return cacheZeroDatabaseInfo;
+            return cacheObject;
         }
 
         public void ClearCache()
         {
             lock (cacheLock)
             {
-                cacheZeroDatabaseInfo = null!;
+                cacheObject = null!;
             }
         }
     }
