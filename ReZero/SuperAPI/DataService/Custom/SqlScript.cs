d3d6a80f02735966f95980339de160003f8dfcc7
sunkaixuan <610262374@qq.com>
2024-06-14 21:45:50 +0800

    支持SQL接口的条件拼接

--- ReZero/SuperAPI/DataService/Custom/SqlScript.cs
+++ ReZero/SuperAPI/DataService/Custom/SqlScript.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ReZero.SuperAPI
@@ -14,25 +15,30 @@ namespace ReZero.SuperAPI
         public async Task<object> ExecuteAction(DataModel dataModel)
         {
             var db = App.GetDbById(dataModel.DataBaseId) ?? App.Db;
-            var sql = dataModel.Sql;
+            var sql = dataModel.Sql + string.Empty;
+            var left = "[[";
+            var right = "]]";
+            var isWhereIf = sql.Contains(left) && sql.Contains(right);
             var pars = new List<SugarParameter>();
-            foreach (var item in dataModel.DefaultParameters??new List<DataModelDefaultParameter>())
+            foreach (var item in dataModel.DefaultParameters ?? new List<DataModelDefaultParameter>())
             {
-                var p = new SugarParameter("@"+item.Name,UtilMethods.ConvertDataByTypeName(item.ValueType, item.Value?.ToString()));
-                if (item.ValueIsReadOnly) 
+                var p = new SugarParameter("@" + item.Name, UtilMethods.ConvertDataByTypeName(item.ValueType, item.Value?.ToString()));
+                if (item.ValueIsReadOnly)
                 {
-                   var claimItem=dataModel.ClaimList.FirstOrDefault(it => it.Key?.ToLower() == item.Name?.ToLower());
+                    var claimItem = dataModel.ClaimList.FirstOrDefault(it => it.Key?.ToLower() == item.Name?.ToLower());
                     p = new SugarParameter("@" + item.Name, claimItem.Value);
                 }
-                if (item.ValueType?.Contains(PubConst.Common_ArrayKey) == true) 
+                if (item.ValueType?.Contains(PubConst.Common_ArrayKey) == true)
                 {
                     var type = item.ValueType.Replace(PubConst.Common_ArrayKey, string.Empty);
-                    var arrayType= typeof(List<>).MakeGenericType( EntityGeneratorManager.GetTypeByString(type));
-                    var value=JsonConvert.DeserializeObject(item.Value?.ToString()?? PubConst.Common_ArrayKey, arrayType);
+                    var arrayType = typeof(List<>).MakeGenericType(EntityGeneratorManager.GetTypeByString(type));
+                    var value = JsonConvert.DeserializeObject(item.Value?.ToString() ?? PubConst.Common_ArrayKey, arrayType);
                     p = new SugarParameter("@" + item.Name, value);
                 }
+                sql = GetSqlByIsWhereIF(sql, left, right, isWhereIf, p);
                 pars.Add(p);
             }
+            sql = GetSqlByIsWhereIf(sql, isWhereIf);
             switch (dataModel.ResultType)
             {
                 case SqlResultType.DataSet:
@@ -40,9 +46,42 @@ namespace ReZero.SuperAPI
                 case SqlResultType.AffectedRows:
                     return await db.Ado.ExecuteCommandAsync(sql, pars);
                 case SqlResultType.Query:
-                default: 
+                default:
                     return await db.Ado.GetDataTableAsync(sql, pars);
             }
+        }
+
+        private static string GetSqlByIsWhereIF(string sql, string left, string right, bool isWhereIf, SugarParameter p)
+        {
+            if (isWhereIf)
+            {
+                var regex = @"\[\[.*?\]\]";
+                var matchCollection = Regex.Matches(sql, regex);
+                foreach (Match math in matchCollection)
+                {
+                    var value = math.Value;
+                    if (value.Contains(p.ParameterName) && string.IsNullOrEmpty(p.Value?.ToString()))
+                    {
+                        sql = sql.Replace(value, string.Empty);
+                    }
+                    else if (value.Contains(p.ParameterName) && !string.IsNullOrEmpty(p.Value?.ToString()))
+                    {
+                        sql = sql.Replace(value, value.Replace(left, null).Replace(right, null));
+                    }
+                }
+            }
+
+            return sql;
+        }
+
+        private static string GetSqlByIsWhereIf(string sql, bool isWhereIf)
+        {
+            if (isWhereIf && Regex.IsMatch(sql.Trim(), "where$", RegexOptions.IgnoreCase))
+            {
+                sql = Regex.Replace(sql.Trim(), "where$", "", RegexOptions.IgnoreCase);
+            }
+
+            return sql;
         }
     }
 }
