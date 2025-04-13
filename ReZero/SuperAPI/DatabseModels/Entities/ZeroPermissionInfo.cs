8213498d31081c8a11da41c19273e5ed832d6181
sunkaixuan <610262374@qq.com>
2025-04-13 14:16:24 +0800

    添加实体

---
+++ ReZero/SuperAPI/DatabseModels/Entities/ZeroPermissionInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReZero.SuperAPI
+{
+    public class ZeroPermissionInfo : DbBase
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string? Name { get; set; } 
+    }
+
+    public class ZeroPermissionMapping : DbBase
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string? UserName { get; set; }
+
+        /// <summary>
+        /// 接口ID
+        /// </summary>
+        public string? InterfaceId { get; set; }
+
+        /// <summary>
+        /// 权限信息ID
+        /// </summary>
+        public string? PermissionInfoId { get; set; }
+    }
+}
