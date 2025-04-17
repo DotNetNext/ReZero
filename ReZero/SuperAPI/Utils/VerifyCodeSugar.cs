6c29b93fdc92b3f634815bc05d2f9a43e401eb8c
sunkaixuan <610262374@qq.com>
2025-04-17 11:23:16 +0800

    跨平台原因移除验证码

--- ReZero/SuperAPI/Utils/VerifyCodeSugar.cs
+++ ReZero/SuperAPI/Utils/VerifyCodeSugar.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing.Drawing2D;
+using System.Collections.Generic; 
 using System.Drawing;
 using System.Text;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
-using System.IO;
-using System.Drawing.Imaging;
-using System.Collections.Concurrent;
-using SkiaSharp;
+using System.IO; 
+using System.Collections.Concurrent; 
 
 namespace ReZero.SuperAPI 
 {
@@ -42,32 +39,7 @@ namespace ReZero.SuperAPI
          
         private static byte[] GenerateImage(string code)
         {
-            int width = 100;
-            int height = 40;
-
-            using var bitmap = new SKBitmap(width, height);
-            using var canvas = new SKCanvas(bitmap);
-            canvas.Clear(SKColors.White);
-
-            // 设置字体样式
-            var typeface = SKTypeface.FromFamilyName("Arial", SKFontStyle.BoldItalic);
-            using var font = new SKFont(typeface, 24); // 替代 Paint.TextSize 和 Paint.Typeface
-
-            // 创建文本图块
-            var blob = SKTextBlob.Create(code, font);
-
-            // 绘制文本
-            using var paint = new SKPaint
-            {
-                Color = SKColors.DarkRed,
-                IsAntialias = true
-            };
-            canvas.DrawText(blob, 5, 30, paint); // 推荐新写法
-
-            // 输出 PNG
-            using var image = SKImage.FromBitmap(bitmap);
-            using var data = image.Encode(SKEncodedImageFormat.Png, 100);
-            return data.ToArray();
+            return new byte[] { };
         }
     }
 }
